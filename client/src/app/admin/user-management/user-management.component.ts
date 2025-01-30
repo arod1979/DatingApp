1f45df98504478b8455f82cbd6b07b007b524533
Allan Rodkin <allan@norwindinteractive.com>
2025-01-30 15:37:58 -0600

    more admin changes

--- client/src/app/admin/user-management/user-management.component.ts
+++ client/src/app/admin/user-management/user-management.component.ts
@@ -42,7 +42,7 @@ export class UserManagementComponent implements OnInit {
         const selectedRoles = this.bsModalRef.content?.selectedRoles;
         if (!this.arrayEqual(selectedRoles!,user.roles)) {
           this.adminService.updateUserRole(user.username, selectedRoles!.join()).subscribe({
-            next: roles =>console.log(roles.toString())
+            next: roles => console.log(roles)
           })
         }
       }
